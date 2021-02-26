b77d7dcf3e480db160c5f038f842d52fdac14c96
NiaMori <Hellc5261@gmail.com>
2021-02-26 16:30:43 +0800

    feat: add hook `useSendDanmu`

---
+++ src/common/use-send-danmu.jsx
@@ -0,0 +1,73 @@
+// @ts-check
+
+import { useCallback } from 'react'
+import { useSelector, shallowEqual } from 'react-redux'
+import { api } from '../api/api'
+import { notification } from 'antd'
+
+// eslint-disable-next-line no-unused-vars
+import * as N from './types/use-send-danmu'
+
+const maxLength = 18
+
+/** @type {(danmu: string) => string[]} */
+const splitDanmu = (danmu) => {
+  if (danmu.length <= maxLength) {
+    return [danmu]
+  } else if (/\s+/.test(danmu)) {
+    const [head, rest] = danmu.split(/\s+/, 2)
+    return [...splitDanmu(head), ...splitDanmu(rest)]
+  } else {
+    const n = Math.ceil(danmu.length / maxLength)
+    const chunkSize = Math.ceil(danmu.length / n)
+
+    const chunks = []
+    for (let i = 0; i < danmu.length; i += chunkSize) {
+      chunks.push(danmu.slice(i, i + chunkSize))
+
+    }
+
+    return chunks
+  }
+}
+
+/** @type {() => N.UseSendDanmuHook} */
+export const useSendDanmu = () => {
+  const { roomNum, user } = useSelector((state) => ({
+    roomNum: state.room.num,
+    user: state.user
+  }), shallowEqual)
+
+  return {
+    send: useCallback(async (danmu) => {
+      for (const chunk of splitDanmu(danmu)) {
+        try {
+          await api.sendDanmu({
+            roomNum,
+            user: 0,
+            danmu: chunk,
+            loc_user: user,
+            color: 16777215,
+            mode: 1
+          })
+        } catch (error) {
+          if (error.name === 'OverheatingError') {
+            notification.error({
+              message: `弹幕 【${chunk}】 发送失败`,
+              description: `错误：弹幕频率过高。建议录入额外的 B 站账号以利用账号池功能`
+            })
+          } else if (error.name === 'DanmuRepeatError') {
+            //
+          } else {
+            notification.error({
+              message: `弹幕 【${chunk}】 发送失败`,
+              description: `错误：${error.message}`
+            })
+          }
+        }
+
+        console.log('sent', chunk)
+      }
+    }, [roomNum, user])
+  }
+}
