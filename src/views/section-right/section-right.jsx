7e5bbe5d74692139f5333ef41726c24071e64931
diannao <55k@outlook.com>
2022-10-15 01:24:21 +0800

    Update section-right.jsx

--- src/views/section-right/section-right.jsx
+++ src/views/section-right/section-right.jsx
@@ -1,4 +1,5 @@
 import React,{Component} from 'react'
+import AdSense from 'react-adsense';
 import {Switch,Route,Redirect} from 'react-router-dom'
 import { Layout } from 'antd';
 
@@ -15,7 +16,8 @@ export default class SectionRight extends Component {
         const {collapsed} = this.props;
         return(
             <Layout className="site-layout" style={{ marginLeft:  collapsed?75:200 }}>
-                <Header className="site-layout-background" style={{ padding: 0 }} />
+                <Header className="site-layout-background" style={{ padding: 0 }} >
+                </Header>
                 <Switch>
                         <Route path='/login' component={Login}/>
                         <Route path='/room' component={Room}/>
@@ -23,7 +25,18 @@ export default class SectionRight extends Component {
                         <Route path='/signup' component={SignUp}/>
                         <Redirect to='/index' />
                 </Switch>
-                <Footer style={{ textAlign: 'center' }}>同传姬 © 2020-2021 同传姬项目开发组</Footer>
+                <Footer style={{ textAlign: 'center' }}>
+                    <AdSense.Google
+                      client='ca-pub-9800395340265192'
+                      slot='9664902413'
+                      style={{ display: 'block' }}
+                      layout='in-article'
+                      format='fluid'
+                    />
+                由于B站近期修改了播放url的referer限制，使得同传姬的播放器无法连接到播放地址，如有任何人能够提供帮助请联系我：qq3149815852<br/>
+                使用中遇到问题请直接向我反馈：qq3149815852<br/>
+                同传姬 © 2020-2021 同传姬项目开发组
+                </Footer>
             </Layout>
         )
     }
